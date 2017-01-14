b75b6c49a6f61d151758772ceeaa06d7ad0d7200
pvosper <pvosper@mac.com>
2017-01-14 08:24:32 -0800

    WIP echo_server first pass

--- echo_client.py
+++ echo_client.py
@@ -45,4 +45,4 @@ if __name__ == '__main__':
         sys.exit(1)
 
     msg = sys.argv[1]
-client(msg)
+    client(msg)
--- echo_server.py
+++ echo_server.py
@@ -1,25 +1,65 @@
+#!/usr/bin/env python3
+
+"""
+Internet Programming in Python: Session 01
+
+Homework
+
+Your homework assignment for this week is to take what you've learned here and
+build a simple "echo" server.
+
+The server should automatically return to any client that connects exactly what
+it receives (it should echo all messages).
+
+You will also write a python script that, when run, will send a message to the
+server and receive the reply, printing it to stdout.
+
+Finally, you'll do all of this so that it can be tested.
+
+"""
+
 import socket
+
 import sys
 
 
 def server(log_buffer=sys.stderr):
     # set an address for our server
     address = ('127.0.0.1', 10000)
-    # TODO: Replace the following line with your code which will instantiate
+    # xTODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
-    # TODO: You may find that if you repeatedly run the server script it fails,
+    # sock = None
+
+    # socket family for IPV4 is "AF_INET"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+
+    # xTODO: You may find that if you repeatedly run the server script it
+    #   fails,
     #       claiming that the port is already used.  You can set an option on
     #       your socket that will fix this problem. We DID NOT talk about this
     #       in class. Find the correct option by reading the very end of the
     #       socket library documentation:
     #       http://docs.python.org/3/library/socket.html#example
 
+    # This is because the previous execution has left the socket in a
+    #   TIME_WAIT state, and can’t be immediately reused.
+    # There is a socket flag to set, in order to prevent this,
+    #   socket.SO_REUSEADDR:
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # s.bind((HOST, PORT))
+
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
 
-    # TODO: bind your new sock 'sock' to the address above and begin to listen
+    # xTODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
+
+    sock.bind(address)
+
+    sock.listen(1)
 
     try:
         # the outer loop controls the creation of new connection sockets. The
@@ -27,12 +67,15 @@ def server(log_buffer=sys.stderr):
         while True:
             print('waiting for a connection', file=log_buffer)
 
-            # TODO: make a new socket when a client connects, call it 'conn',
+            # xTODO: make a new socket when a client connects, call it 'conn',
             #       at the same time you should be able to get the address of
             #       the client so we can report it below.  Replace the
             #       following line with your code. It is only here to prevent
             #       syntax errors
-            addr = ('bar', 'baz')
+            # addr = ('bar', 'baz')
+
+            conn, addr = sock.accept()
+
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
 
@@ -40,37 +83,55 @@ def server(log_buffer=sys.stderr):
                 # buffers.  When a complete message has been received, the
                 # loop will exit
                 while True:
-                    # TODO: receive 16 bytes of data from the client. Store
+                    # xTODO: receive 16 bytes of data from the client. Store
                     #       the data you receive as 'data'.  Replace the
                     #       following line with your code.  It's only here as
                     #       a placeholder to prevent an error in string
                     #       formatting
-                    data = b''
+                    # data = b''
+
+                    data = conn.recv(16)
+
                     print('received "{0}"'.format(data.decode('utf8')))
-                    # TODO: Send the data you received back to the client, log
+
+                    # xTODO: Send the data you received back to the client, log
                     # the fact using the print statement here.  It will help in
                     # debugging problems.
+
+                    conn.sendall(data.encode('utf8'))
+
                     print('sent "{0}"'.format(data.decode('utf8')))
-                    # TODO: Check here to see if the message you've received is
+
+                    # xTODO: Check here to see if the message you've received
+                    #   is
                     # complete.  If it is, break out of this inner loop.
 
+                    if len(data) < 16:
+                        break
+
             finally:
-                # TODO: When the inner loop exits, this 'finally' clause will
+                # xTODO: When the inner loop exits, this 'finally' clause will
                 #       be hit. Use that opportunity to close the socket you
                 #       created above when a client connected.
+
+                sock.close()
+
                 print(
                     'echo complete, client connection closed', file=log_buffer
                 )
 
     except KeyboardInterrupt:
-        # TODO: Use the python KeyboardInterrupt exception as a signal to
+        # xTODO: Use the python KeyboardInterrupt exception as a signal to
         #       close the server socket and exit from the server function.
         #       Replace the call to `pass` below, which is only there to
         #       prevent syntax problems
-        pass
+        # pass
+
+        sock.close()
+
         print('quitting echo server', file=log_buffer)
 
 
 if __name__ == '__main__':
     server()
-sys.exit(0)
+    sys.exit(0)
